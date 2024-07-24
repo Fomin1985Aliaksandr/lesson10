dfb61a6338721ff5cbe7b4e85f177c59db7e76f0
aliaksandr <aliaksandr_fomin@list.ru>
2024-07-24 19:49:23 +0300

    SoftAsser+DataProvider+Listner

---
+++ src/main/java/org/example/tests/TestClass.java
@@ -0,0 +1,37 @@
+package org.example.tests;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrom.driver", "src/main/java/resources/driver/chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+                public void openGoogle(){
+
+            driver.get("https://www.google.ru/");
+  System.out.println("Успех");
+
+        }
+@AfterMethod
+public void tearDown(){
+        driver.quit();
+
+}
+
+
+    }
+
+
