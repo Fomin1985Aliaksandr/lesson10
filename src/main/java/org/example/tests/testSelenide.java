a64cab73e211ad1f8bcb58fea9f69cbe60d615e6
aliaksandr <96591290+Fomin1985Aliaksandr@users.noreply.github.com>
2024-08-08 07:31:59 +0300

    Update testSelenide.java

--- src/main/java/org/example/tests/testSelenide.java
+++ src/main/java/org/example/tests/testSelenide.java
@@ -1,16 +1,58 @@
 package org.example.tests;
-
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selenide.open;
-
+import java.io.File;
+import static com.codeborne.selenide.Selenide.*;
 public class testSelenide {
 
 
+
+SelenideElement inputFile = $x("//input[@type='file']");
+SelenideElement BlueBotton = $x("//button[@type='button' and @class='btn class1 btn-primary btn-test']");
+
+    @BeforeTest
+    public void testConf(){
+
+
+            Configuration.browser=System.setProperty("browser","chrome");
+            Configuration.headless=false;
+            Configuration.downloadsFolder= "src/main/java/resources/downloadDirectory";
+    }
+
+
+
     @Test
-public void testSelenide(){
-    open("https://www.google.by/");
+    public void testSelenide(){
+        open("http://www.uitestingplayground.com/upload");
+ File fileToUpload = new File("C:\\Program Files\\Java\\avtotest\\untitled7maven\\src\\main\\java\\resources\\toUpload\\upload.txt ");
+Selenide.switchTo().frame(0);
+inputFile.uploadFile(fileToUpload);
+//$("//input[@type='file']").uploadFile(fileToUpload)
+    }
+
+
+
+
+@Test (testName ="classAtribute")
+
+public void classAtribute () {
+
+        open("http://www.uitestingplayground.com/classattr");
+     $("//button[@type='button' and @class='btn class1 btn-primary btn-test']");
+
 
 }
 
 }
+
+
+//    $x("//button[@type='button' and @class='btn class1 btn-primary btn-test']")
